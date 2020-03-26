b00d757125ad7b58959dab87eb87c8aa25419341
Flavius-Gabriel Brandeu <flavius.brandeu@gmail.com>
2020-03-26 20:32:51 +0200

    SM-1 Solved the comments. You can now view the students after you login as a teacher

--- src/loose/oose/fis/lab/student/manager/Main.java
+++ src/loose/oose/fis/lab/student/manager/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("fxml/login.fxml"));
         primaryStage.setTitle("FIS - Student Manager");
-        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.setScene(new Scene(root, 630, 400));
         primaryStage.show();
     }
 
